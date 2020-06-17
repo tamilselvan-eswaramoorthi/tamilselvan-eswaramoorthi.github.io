4c54b0f4844e75b9a9b081f6eec30205768a28a9
Tamilselvan <tamilselvan.ei15@bitsathy.ac.in>
2020-06-17 13:39:16 +0530

    added style

--- img/python_script.py
+++ img/python_script.py
@@ -1,7 +1,7 @@
 import cv2
 
-img = cv2.imread('/home/lenovo/Desktop/js/website/tamilselvan-eswaramoorthi.github.io/img/xnr9649_how-to-develop-a-signature-event.jpg')
+img = cv2.imread('/home/lenovo/Desktop/js/website/tamilselvan-eswaramoorthi.github.io/img/1.jpg')
 
 img = cv2.resize(img, (700, 467))
 
-cv2.imwrite('2.png',img)
+cv2.imwrite('1.png',img)
